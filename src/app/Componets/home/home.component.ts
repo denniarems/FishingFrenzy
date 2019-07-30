ad32e114fc10db1918b51f4b0a8eb532e3c6116d
denniarems <denniarems@gmail.com>
2019-07-30 17:12:52 +0530

    added web3 support

--- src/app/Componets/home/home.component.ts
+++ src/app/Componets/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Web3Service } from '../../Services/web3/web3.service';
 // import { WalletLoginService } from 'src/app/Services/WalletLogin/wallet-login.service';
 @Component({
   selector: 'app-home',
@@ -7,11 +8,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(private route: Router) {}
+  constructor(private route: Router, private web3service: Web3Service) {}
 
   ngOnInit() {}
 
   login = () => {
+    this.web3service.web3login();
     console.log('Login Button Pressed');
     // this._web3Login.MetamaskLogin();
     this.route.navigateByUrl('/playground');
--- src/app/Services/web3/web3.service.ts
+++ src/app/Services/web3/web3.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@angular/core';
-// import Web3 from 'web3';
+import contract from 'truffle-contract';
+import { Subject } from 'rxjs';
+
+declare let require: any;
+
+const Web3 = require('web3');
+
+// const landRegistry_artifacts = require('../../build/contracts/landRegistration.json');
+declare let window: any;
 @Injectable({
   providedIn: 'root'
 })
 export class Web3Service {
-  constructor() {}
+  public web3: any;
+
+  public async web3login() {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof window.web3 !== 'undefined') {
+      // Use Mist/MetaMask's provider
+      await window.ethereum.enable();
+      this.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.log('No web3? You should consider trying MetaMask!');
+
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      this.web3 = new Web3(
+        new Web3.providers.HttpProvider('http://127.0.0.1:8545')
+      );
+    }
+
+    this.getAccounts();
+  }
+
+  private getAccounts() {
+    this.web3.eth.getAccounts((err, accs) => {
+      if (err != null) {
+        console.warn('There was an error fetching your accounts.');
+        return;
+      }
+
+      // Get the initial account balance so it can be displayed.
+      if (accs.length === 0) {
+        console.warn(
+          'Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.'
+        );
+        return;
+      } else {
+        console.log('account array from service page', accs);
+      }
+    });
+  }
 }
