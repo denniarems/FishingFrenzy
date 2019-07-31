2db87853c9194904dabbdcfda5baef8a5d2d25d0
Aswath K <aswathashii@gmail.com>
2019-07-31 11:39:57 +0530

    contract addes buyrod

---
+++ contracts/BuyRod.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+contract BuyRode {
+    
+    enum rodetype {male, female, other}
+    
+    struct property {
+       
+        uint power;
+       
+        rodetype mytype;
+    }
+    
+    mapping(uint => property) rode;
+    
+    function setRode(uint _roll,  uint _power, rodetype _mytype) public {
+        rode[_roll] = property(_power, _mytype );
+    }
+    
+    function getRode(uint _roll) public view returns ( uint _power,  rodetype _mytype){
+        _power = rode[_roll].power;
+        
+        _mytype = rode[_roll].mytype;
+    }
+    
+}
