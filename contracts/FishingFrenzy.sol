76c7dcb61f7ecde8e13fd7236c07d0b4ce588293
Aswath K <aswathashii@gmail.com>
2019-08-02 23:59:53 +0530

    contact added

--- contracts/FishingFrenzy.sol
+++ contracts/FishingFrenzy.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.5.0;
 contract FishingFrenzy{
-    struct FishRod{
-        uint8 power;
-    }
+    enum Rarity{common,rare,epic}
+    
     struct Fish{
+        uint8 weight;
+        Rarity fishRarity;
+    }
+    struct FishRod{
         uint8 power;
     }
     struct User{
@@ -12,11 +15,20 @@ contract FishingFrenzy{
         Fish fish;
     }
     mapping(address => User) Userdeails;
-    function GetUserDetails() public view returns(bool _isexsiting){
-        _isexsiting  =Userdeails[msg.sender].ExistingUser;
+    function GetUserDetails() public view returns(bool _isexsiting, uint8 _weight ,Rarity _rarity){
+        _isexsiting = Userdeails[msg.sender].ExistingUser;
+        _weight=Userdeails[msg.sender].fish.weight;
+        _rarity=Userdeails[msg.sender].fish.fishRarity;
     }
     function SetUserDetails() public{
         Userdeails[msg.sender].ExistingUser = true;
     }
-
+    function SetRodDetails(uint8 _power)public{
+        Userdeails[msg.sender].fishRod.power= _power;
+    }
+    function SetFishDetails(uint8 _weight,Rarity _Rarity) public{
+        Userdeails[msg.sender].fish.weight=_weight;
+         Userdeails[msg.sender].fish.fishRarity=_Rarity;
+        
+    }
 }
---
+++ contracts/studymeterial/ItemListing.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.25;
+
+import "./BazaarItemListing.sol";
+
+contract ItemListing
+{
+    enum StateType { ItemAvailable, ItemSold }
+
+    StateType public State;
+
+    address public Seller;
+    address public InstanceBuyer;
+    address public ParentContract;
+    string public ItemName;
+    int public ItemPrice;
+    address public PartyA;
+    address public PartyB;
+    
+
+    constructor(string itemName, int itemPrice, address seller, address parentContractAddress, address partyA, address partyB) public {
+        Seller = seller;
+        ParentContract = parentContractAddress;
+        ItemName = itemName;
+        ItemPrice = itemPrice;
+
+        PartyA = partyA;
+        PartyB = partyB;
+
+        State = StateType.ItemAvailable;
+    }
+
+    function BuyItem() public
+    {
+        InstanceBuyer = msg.sender;
+
+        // ensure that the buyer is not the seller
+        if (Seller == InstanceBuyer) {
+            revert();
+        }
+
+        Bazaar bazaar = Bazaar(ParentContract);
+
+        // check Buyer's balance
+        if (!bazaar.HasBalance(InstanceBuyer, ItemPrice)) {
+            revert();
+        }
+
+        // indicate item bought by updating seller and buyer balances
+        bazaar.UpdateBalance(Seller, InstanceBuyer, ItemPrice);
+
+        State = StateType.ItemSold;
+    }
+}
