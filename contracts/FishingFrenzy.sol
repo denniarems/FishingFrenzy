f4e4dcf804b0a9d2e57355af5dbb90742bfe670d
Aswath K <aswathashii@gmail.com>
2019-08-03 00:04:19 +0530

    update

--- contracts/FishingFrenzy.sol
+++ contracts/FishingFrenzy.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 contract FishingFrenzy{
+
     enum Rarity{common,rare,epic}
-    
     struct Fish{
         uint8 weight;
         Rarity fishRarity;
@@ -17,18 +17,18 @@ contract FishingFrenzy{
     mapping(address => User) Userdeails;
     function GetUserDetails() public view returns(bool _isexsiting, uint8 _weight ,Rarity _rarity){
         _isexsiting = Userdeails[msg.sender].ExistingUser;
-        _weight=Userdeails[msg.sender].fish.weight;
-        _rarity=Userdeails[msg.sender].fish.fishRarity;
+        _weight = Userdeails[msg.sender].fish.weight;
+        _rarity = Userdeails[msg.sender].fish.fishRarity;
     }
     function SetUserDetails() public{
         Userdeails[msg.sender].ExistingUser = true;
     }
     function SetRodDetails(uint8 _power)public{
-        Userdeails[msg.sender].fishRod.power= _power;
+        Userdeails[msg.sender].fishRod.power = _power;
     }
     function SetFishDetails(uint8 _weight,Rarity _Rarity) public{
-        Userdeails[msg.sender].fish.weight=_weight;
-         Userdeails[msg.sender].fish.fishRarity=_Rarity;
+        Userdeails[msg.sender].fish.weight = _weight;
+         Userdeails[msg.sender].fish.fishRarity = _Rarity;
         
     }
      
