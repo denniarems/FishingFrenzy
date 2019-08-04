fe1205cc4a00c3c8398bf478eaaa46d5e8c9c8a6
denniarems <denniarems@gmail.com>
2019-08-05 00:54:42 +0530

    fixxxxx

---
+++ contracts/FishRod.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.0;
+
+
+import './openzeppelin-solidity/contracts/math/SafeMath.sol';
+// import './SafeMath.sol';
+contract FishRod {
+    using SafeMath for uint256;
+    struct  Rod{
+        uint256 Level;
+        uint256 Power;
+        uint256 CurrentPrice;
+        uint256 NextPrice;
+    }
+mapping (address=>Rod) public UsersRod;
+
+function FirstUserInitialRod() public{
+    if(UsersRod[msg.sender].Level == 0){
+        UsersRod[msg.sender].Level = 1;
+        UsersRod[msg.sender].Power = 10;
+        UsersRod[msg.sender].CurrentPrice = 10;
+        UsersRod[msg.sender].NextPrice = 20;
+}}
+    function UpgradeFishRod() public{
+        assert(UsersRod[msg.sender].Level!=0);
+        uint256 Price = UsersRod[msg.sender].NextPrice;
+        UsersRod[msg.sender].Level = UsersRod[msg.sender].Level.add(1);
+        UsersRod[msg.sender].Power = UsersRod[msg.sender].Power.add(10);
+        UsersRod[msg.sender].NextPrice = UsersRod[msg.sender].NextPrice.add(UsersRod[msg.sender].CurrentPrice);
+        UsersRod[msg.sender].CurrentPrice = Price;
+    }
+}
