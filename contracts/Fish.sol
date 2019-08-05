f9675e8f3688402f5a29e9626fdc62998be5e4df
Aswath K <aswathashii@gmail.com>
2019-08-06 00:24:15 +0530

    fish contract updated

--- contracts/Fish.sol
+++ contracts/Fish.sol
@@ -1,20 +1,36 @@
 pragma solidity ^0.5.0;
 
-contract Fish{
-    struct FishModel{
+
+import './openzeppelin-solidity/contracts/math/SafeMath.sol';
+
+contract Fish  {
+    using SafeMath for uint256;
+
+    struct FishModel  {
         uint256 Rarity;
         uint256 Weight;
         address Owner;
         uint256 Price;
     }
     FishModel public ThisFish;
-    
-    
-        constructor(address _owner,uint256 _rarity,uint256 _weight,uint256 price) public{
+
+        constructor(address _owner,uint256 _weight,uint256 _price) public   {
         ThisFish.Owner = _owner;
+        ThisFish.Rarity = Rarity();
         ThisFish.Weight = _weight;
-        ThisFish.Rarity = _rarity;
-        ThisFish.Price = price;
+        ThisFish.Price = _price;
+    }
+
+    function Rarity() public view returns(uint256)  {
+        if ((block.gaslimit+block.difficulty+now).mod(100)==0) {
+            return 0;
+        } 
+        if ((block.gaslimit+block.difficulty+now).mod(10)==0) {
+            return 1;
+        } else{
+            return 2;
+
+        } 
     }
 
 }
