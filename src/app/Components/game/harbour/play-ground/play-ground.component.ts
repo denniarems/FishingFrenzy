9d08c4f0ea55e6ec184ea887454a95e78843ecf9
Aswath K <aswathashii@gmail.com>
2019-08-12 20:37:51 +0530

    updations frontend

--- src/app/Components/game/harbour/play-ground/play-ground.component.ts
+++ src/app/Components/game/harbour/play-ground/play-ground.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare let window: any;
+declare let web3: any;
 
 @Component({
   selector: 'app-play-ground',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play-ground.component.scss']
 })
 export class PlayGroundComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  play = () => {
+    const ContractJSON = require('../../../../../../build/contracts/FrenzyFish.json');
+    const contractsAddress = ContractJSON.networks['5777'].address;
+    const abi = ContractJSON.abi;
+    const Contract = new window.web3.eth.Contract(abi, contractsAddress);
+    web3.eth.getAccounts((err, accs) => {
+      Contract.methods
+        .Fishing()
+        .send({
+          from: accs[0],
+          gas: 3000000
+        })
+        .then(s => {
+          console.log(s);
+        });
+    });
   }
-
 }
