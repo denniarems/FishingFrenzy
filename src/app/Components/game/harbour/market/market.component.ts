3bd89e7891ec02d9e43806ecabc43d0b932a2e01
Dennis Sam <denniarems@gmail.com>
2019-09-10 20:49:13 +0530

    market listing

--- src/app/Components/game/harbour/market/market.component.ts
+++ src/app/Components/game/harbour/market/market.component.ts
@@ -6,11 +6,6 @@ import { FishModel } from 'src/app/Models/fish.model';
 import { FishService } from 'src/app/Services/fish/fish.service';
 import { OrderModel } from 'src/app/Models/order.model';
 
-
-
-
-
-
 @Component({
   selector: 'app-market',
   templateUrl: './market.component.html',
@@ -22,12 +17,10 @@ export class MarketComponent implements OnInit {
   BuyOrderFishes: OrderModel[] = [];
   MyOrderFishes: OrderModel[] = [];
   tempf: OrderModel[] = [];
-
   constructor(
     private _appService: AppService,
     private _fishService: FishService) {
   }
-
 
   ngOnInit() {
     this.Contract = this._appService.getFrenzyFishContract();
@@ -44,91 +37,111 @@ export class MarketComponent implements OnInit {
     });
     this.Orders();
   }
-  async Orders() {
+   Orders() {
     this.Contract.methods
     .ListMarketOrders()
     .call({from: this.account})
     .then(async (Orders: { [s: string]: {}; } | ArrayLike<{}>) => {
-      this.tempf = await this._fishService.listOrders(Orders);
-      await this.listingBuyOrders(this.tempf);
-      // await this.listingMyOrders(this.tempf);
+      this.tempf =  this._fishService.listOrders(Orders);
+      this.listingBuyOrders(this.tempf);
+      this.listingMyOrders(this.tempf);
       });
-  }
+    }
 
- async listingBuyOrders(order: OrderModel[])  {
-    let buyOrder: OrderModel[];
-    let index = 0;
-    buyOrder = new Promise((resolve) => {
-      this.Contract.methods
-      .ListAllFishes()
+listingBuyOrders(order: OrderModel[])  {
+   let buyOrder: OrderModel[] = [];
+   let index = 0;
+   new Promise((resolve) => {
+     this.Contract.methods
+     .ListAllFishes()
       .call({from: this.account})
-      .then((async ( fishes: any) => {
-        if (!(fishes.length === 0)) {
-          fishes.forEach(async (fishAddress: any) => {
-            await this.Contract.methods
-              .GetFishDetails(fishAddress)
-              .call({from: this.account})
-              .then(async (fish: any) => {
-                order.forEach(orderfish => {
-                  if (orderfish.fish === fishAddress && fish._onOrder) {
-                    buyOrder[index] = orderfish;
-                    index++;
-                  }
-                });
+      .then((( fishes: any) => {
+      if (fishes.length == 0) {
+        console.log('1');
+        resolve(order);
+      } else {
+        console.log('2');
+        fishes.forEach((fishAddress: any) => {
+        for (const orderfish of order) {
+          this.Contract.methods
+          .GetFishDetails(fishAddress)
+          .call({from: this.account})
+          .then((fish: any) => {
+            if (fishes.indexOf(orderfish.fish) == -1) {
+              if (buyOrder.indexOf(orderfish) == -1) {
+                buyOrder[index] = orderfish;
+                index++;
               }
-              );
+            }
           });
-          resolve(buyOrder);
-        } else {
-          resolve(order);
         }
-      }));
-    });
-    buyOrder.then((result) => {
-      console.log(buyOrder);
-      this._appService.updateMyOrderList(buyOrder);
-    })
-  }
-  // listingMyOrders(order: OrderModel[])  {
-  //   let count = 0;
-  //   let fcount = 0;
-  //   this.Contract.methods
-  //     .ListAllFishes()
-  //     .call({from: this.account})
-  //     .then((address: { forEach: (arg0: (fishAddress: any) => void) => void; }) => {
-  //       address.forEach((fishAddress: string) => {
-  //         this.Contract.methods
-  //           .GetFishDetails(fishAddress)
-  //           .call({from: this.account})
-  //           .then((fish: any) => {
-  //             if ( fish._onOrder) {
-  //               this.tempf[fcount] = this._fishService.listFish(count, fishAddress, fish);
-  //               fcount++;
-  //             }
-  //             count++;
-  //           }
-  //           );
-  //       });
-  //       this._appService.updateMyOrderList(this.tempf);
-  //     });
-  //   return this.tempf;
-  // }
-//   cancelOrder = (fish: OrderModel) => {
-//     console.log(fish);
+          });
+      resolve(buyOrder);
+      }}));
+    }).then((result: OrderModel[] ) => {
 
-//     this.Contract.methods
-//     .CancelSellFishOrder(fish.fish, fish.orderid)
-//     .send({
-//       from: this.account,
-//       gas: 3000000
-//     }).then((success: any) => {
-//       console.log(success);
-//       // if (success.status) {
-//       //   alert('Sell Order Placed SuccessFully')
-//       // }
-//   });
-// }
-  buyFish = (fish: OrderModel) => {
-    alert('Buy Order Coming Soon!!!!!');
+      this._appService.updateBuyOrderList(result);
+    }
+    );
   }
+
+  listingMyOrders(order: OrderModel[])  {
+   let cancelOrder: OrderModel[] = [];
+   let index = 0;
+   new Promise((resolve) => {
+     this.Contract.methods
+     .ListAllFishes()
+      .call({from: this.account})
+      .then((( fishes: any) => {
+      if (fishes.length == 0) {
+        resolve(order);
+      } else {
+      fishes.forEach((fishAddress: any) => {
+        for (const orderfish of order) {
+          this.Contract.methods
+          .GetFishDetails(fishAddress)
+          .call({from: this.account})
+          .then((fish: any) => {
+            if (fishes.indexOf(orderfish.fish) >= 0 && fish._onOrder) {
+              if (cancelOrder.indexOf(orderfish) == -1) {
+                cancelOrder[index] = orderfish;
+                index++;
+              }
+            }
+          });
+        }
+          });
+      resolve(cancelOrder);
+      }}));
+    }).then((result: OrderModel[] ) => {
+
+      this._appService.updateMyOrderList(result);
+    }
+    );
+  }
+
+  cancelOrder = (fish: OrderModel) => {
+    console.log(fish);
+
+    this.Contract.methods
+    .CancelSellFishOrder(fish.fish, fish.orderid)
+    .send({
+      from: this.account,
+      gas: 3000000
+    }).then((success: any) => {
+      console.log(success);
+  });
+}
+  buyFish = (fish: OrderModel) => {
+    console.log(fish);
+    this.Contract.methods
+    .BuyFish(fish.fish, fish.orderid)
+    .send({
+      from: this.account,
+      gas: 3000000
+    }).then((success: any) => {
+      console.log(success);
+  });
+}
+
 }
